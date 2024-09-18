a54eae4e6891e6301cedc7529190756a7ae7bb1c
NaclFire <nacl0312@163.com>
2024-09-18 15:13:48 +0800

    1.PhotoView代码放入本地

--- photoselector/src/main/java/com/fire/photoselector/adapter/PhotoViewAdapter.java
+++ photoselector/src/main/java/com/fire/photoselector/adapter/PhotoViewAdapter.java
@@ -7,10 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.bm.library.PhotoView;
 import com.bumptech.glide.Glide;
 import com.fire.photoselector.R;
 import com.fire.photoselector.models.PhotoSelectorSetting;
+import com.fire.photoselector.view.photoview.PhotoView;
 
 import java.util.ArrayList;
 import java.util.List;
